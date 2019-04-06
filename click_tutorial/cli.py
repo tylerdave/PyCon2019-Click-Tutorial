3c9408ad17381bf6674066ef4b0c411092f7ec47
Dave Forgac <tylerdave@tylerdave.com>
2019-04-06 18:45:04 -0400

    add verify stub

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -5,13 +5,14 @@ import sys
 import click
 
 
-@click.command()
+@click.group()
 def main(args=None):
-    """Console script for click_tutorial."""
-    click.echo("Replace this message by putting your code into "
-               "click_tutorial.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+    """Click tutorial runner."""
+
+@main.command()
+def verify():
+    """Verify that your environment is set up correctly."""
+    click.echo("All good!")
 
 
 if __name__ == "__main__":
