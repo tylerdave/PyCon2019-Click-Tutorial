5c332c86628f313ba35dfc5fe0c52f95863e19d1
Dave Forgac <tylerdave@tylerdave.com>
2019-04-24 23:48:44 -0400

    add lesson 03

---
+++ lessons/part_01/solutions/cli_03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import click
+
+@click.argument('names', nargs=-1)
+@click.option('--greeting', '-g', default= "Hello")
+@click.option('--question/--no-question')
+@click.command()
+def cli(names, greeting, question):
+    if question:
+        punctuation = '?'
+    else:
+        punctuation = "!"
+    for name in names:
+        print("{}, {}{}".format(greeting, name, punctuation))
+
+if __name__ == "__main__":
+    cli()
--- lessons/part_01/tests/03_opts.py
+++ lessons/part_01/tests/03_opts.py
@@ -3,46 +3,23 @@ from .base import BaseTutorialLesson
 
 class TestTutorialBasicOptions(BaseTutorialLesson):
 
-    def test_00_cli_with_single_option(self):
-        result = self.run_command(['--single', 'single-value'])
-        assert result.output.startswith('single: single-value\n')
+    def test_00_greeting_option(self):
+        result = self.run_command(['--greeting', 'Ahoy', 'Tutorial'])
+        assert result.output == 'Ahoy, Tutorial!\n'
 
-    def test_01_cli_with_single_short_option(self):
-        result = self.run_command(['-s', 'single-value'])
-        assert result.output.startswith('single: single-value\n')
+    def test_01_greeting_short_option(self):
+        result = self.run_command(['-g', 'Ahoy', 'Tutorial'])
+        assert result.output == 'Ahoy, Tutorial!\n'
 
-    def test_02_cli_with_single_option_not_passed(self):
-        result = self.run_command()
-        assert result.output.startswith('single: None\n')
-
-    def test_03_cli_with_multiple_value_options(self):
-        result = self.run_command(['--multi', 'first-value', '--multi', 'second-value'])
-        assert 'multi: first-value\nmulti: second-value' in result.output
-
-    def test_04_cli_with_flag_on(self):
-        result = self.run_command(['--enable-feature'])
-        assert 'enable_feature: True\n' in result.output
-
-    def test_05_cli_with_flag_off(self):
-        result = self.run_command(['--no-enable-feature'])
-        assert 'enable_feature: False\n' in result.output
-
-    def test_06_cli_with_flag_not_passed(self):
-        result = self.run_command()
-        assert 'enable_feature: False\n' in result.output
-
-    def test_07_cli_with_explicit_flag(self):
-        result = self.run_command(['--flag'])
-        assert 'flag: True' in result.output
-
-    def test_08_cli_with_explicit_flag_not_passed(self):
-        result = self.run_command()
-        assert 'flag: False' in result.output
-
-    def test_09_cli_with_counting_option_not_passed(self):
-        result = self.run_command()
-        assert 'verbose: 0' in result.output
-
-    def test_10_cli_with_counting_option_passed_many_times(self):
-        result = self.run_command(['-vvvv', '-v', '--verbose'])
-        assert 'verbose: 6' in result.output
+    def test_02_greeting_default(self):
+        result = self.run_command(['Tutorial'])
+        assert result.output == "Hello, Tutorial!\n"
+ 
+    def test_03_question_option(self):
+        result = self.run_command(['--question', 'Tutorial'])
+        assert result.output == "Hello, Tutorial?\n"
+         
+    def test_04_no_question_option(self):
+        result = self.run_command(['--no-question', 'Tutorial'])
+        assert result.output == "Hello, Tutorial!\n"
+ 
