96e2e2df621613901cd9750f93b0f07c74a7fb0b
Dave Forgac <tylerdave@tylerdave.com>
2019-04-26 11:18:03 -0400

    add help lesson

---
+++ lessons/part_01/solutions/cli_04_help.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import click
+
+@click.argument('names', nargs=-1)
+@click.option('--greeting', '-g', default= "Hello", help="The greeting to display.")
+@click.option('--question/--no-question', help="Make the greeting a question.")
+@click.command(name='greet')
+def cli(names, greeting, question):
+    """Displays a greeting."""
+    if question:
+        punctuation = '?'
+    else:
+        punctuation = "!"
+    for name in names:
+        print("{}, {}{}".format(greeting, name, punctuation))
+
+if __name__ == "__main__":
+    cli()
