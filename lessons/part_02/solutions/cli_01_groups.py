d49b29568395fb6610a0af1ec2596d8f41710fb7
Dave Forgac <tylerdave@tylerdave.com>
2019-04-27 16:38:16 -0400

    add solution 01

---
+++ lessons/part_02/solutions/cli_01_groups.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import click
+
+
+@click.group()
+def cli():
+    """Displays greetings."""
+
+@cli.command()
+def hello():
+    click.echo("Hello!")
+
+
+if __name__ == "__main__":
+    cli()
