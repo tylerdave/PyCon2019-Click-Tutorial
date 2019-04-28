f2c911def6c51852662a74207a0e0476c01040f9
Dave Forgac <tylerdave@tylerdave.com>
2019-04-28 15:25:26 -0400

    blacken

--- lessons/part_02/solutions/cli_02_contexts.py
+++ lessons/part_02/solutions/cli_02_contexts.py
@@ -4,27 +4,29 @@ import click
 
 
 @click.group()
-@click.option('--verbose', is_flag=True)
+@click.option("--verbose", is_flag=True)
 @click.pass_context
 def cli(ctx, verbose):
     """Displays greetings."""
     ctx.ensure_object(dict)
-    ctx.obj['VERBOSE'] = verbose
+    ctx.obj["VERBOSE"] = verbose
+
 
 @cli.command()
 @click.pass_context
 def hello(ctx):
-    verbose = ctx.obj.get('VERBOSE')
+    verbose = ctx.obj.get("VERBOSE")
     if verbose:
-        click.echo('VERBOSE is on', err=True)
+        click.echo("VERBOSE is on", err=True)
     click.echo("Hello!")
+
 
 @cli.command()
 @click.pass_obj
 def goodbye(obj):
-    verbose = obj.get('VERBOSE')
+    verbose = obj.get("VERBOSE")
     if verbose:
-        click.echo('VERBOSE is on', err=True)
+        click.echo("VERBOSE is on", err=True)
     click.echo("Goodbye!")
 
 
