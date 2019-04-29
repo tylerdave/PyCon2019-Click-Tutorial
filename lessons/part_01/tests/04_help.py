be7dff8b4dc0e558c8a720472bbe9ae8dbe54b85
Dave Forgac <tylerdave@tylerdave.com>
2019-04-29 01:26:34 -0400

    fix tests

--- lessons/part_01/tests/04_help.py
+++ lessons/part_01/tests/04_help.py
@@ -6,17 +6,17 @@ from .base import BaseTutorialLesson
 class TestTutorialBasicUsageDocumentation(BaseTutorialLesson):
     def test_00_cli_gets_help_output_from_docstring(self):
         result = self.run_command(["--help"])
-        assert result.output.startswith("Usage: greet")
+        assert result.output.startswith("Usage: ")
         assert "Displays a greeting." in result.output
 
-    def test_01_cli_includes_help_text_from_option(self):
+    def test_01_cli_includes_help_text_from_question_option(self):
         result = self.run_command(["--help"])
         assert re.search(
             r"--question / --no-question\W+Make the greeting a question\.",
             result.output,
         )
 
-    def test_03_cli_with_subcommand_shows_short_help(self):
+    def test_02_cli_includes_help_text_from_greeting_option(self):
         result = self.run_command(["--help"])
         assert re.search(
             r"-g, --greeting TEXT\W+The greeting to display.", result.output
